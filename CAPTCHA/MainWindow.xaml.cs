dfcb72f1905865cec53f3689274632b8e7251a9d
Aynu1 <aynursafin2018@gmail.com>
2023-04-18 02:18:38 +0300

    test

---
+++ CAPTCHA/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CAPTCHA
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string captchaText;
+        public MainWindow()
+        {
+            InitializeComponent();
+            GenerateCaptcha();
+            captchaTextBox.Focus();
+        }
+        private void GenerateCaptcha()
+        {
+            Random random = new Random();
+            captchaText = "";
+
+            for (int i = 0; i < 6; i++)
+            {
+                captchaText += (char)random.Next(65, 90);
+            }
+
+            captchaTextBlock.Text = captchaText;
+        }
+
+        private void submitButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (captchaTextBox.Text == captchaText)
+            {
+                MessageBox.Show("CAPTCHA passed!");
+                GenerateCaptcha();
+                captchaTextBox.Clear();
+                captchaTextBox.Focus();
+            }
+            else
+            {
+                MessageBox.Show("CAPTCHA failed! Please try again.");
+                GenerateCaptcha();
+                captchaTextBox.Clear();
+                captchaTextBox.Focus();
+            }
+        }
+    }
+}
